56dbde7956c4e17c714ae0945084c2b92fbb46b0
mai2161 <maikomar@yahoo.com>
2025-12-29 16:56:17 -0500

    Added mlkem python implementation and tested intermediate algorithms

---
+++ src/mlkem/Internal_kpke.py
@@ -0,0 +1,152 @@
+from .auxiliaries import (
+    SampleNTT,
+    PRF_eta,
+    SamplePolyCBD_eta,
+    NTT,
+    NTT_inv,
+    MultiplyNTTs,
+    ByteEncode_d,
+    ByteDecode_d,
+    Compress_d,
+    Decompress_d,
+    G,
+    q,
+)
+
+# FIPS203 Algorithm 13
+def KPKE_KeyGen(d: bytes, k: int, eta1: int) -> (bytes,bytes):
+    # Encode k into the minimal number of bytes (at least 1) to avoid
+    # 'bytes must be in range(0, 256)' when k > 255.
+    k_bytes_len = max(1, (k.bit_length() + 7) // 8)
+    rho, sigma = G(d + k.to_bytes(k_bytes_len, 'big'))
+
+    A_hat = [[None for _ in range(k)] for _ in range(k)]
+  
+    for i in range(k):
+        for j in range(k):
+            A_hat[i][j] = SampleNTT(rho + bytes([i]) + bytes([j]))
+            
+    Nctr = 0
+    
+    s = []
+    for _ in range(k):
+        s.append(SamplePolyCBD_eta(
+            PRF_eta(
+                eta1,
+                sigma,
+                bytes([Nctr])
+                ),
+            eta1)
+            )
+        Nctr += 1
+        
+    e = []
+    for _ in range(k):
+        e.append(SamplePolyCBD_eta(
+            PRF_eta(
+                eta1,
+                sigma,
+                bytes([Nctr])
+                ),
+            eta1)
+            )
+        Nctr += 1
+
+    s_hat = [NTT(poly) for poly in s]
+    e_hat = [NTT(poly) for poly in e]
+
+    t_hat = []
+    for i in range(k):
+        aux = list(e_hat[i])
+        for j in range(k):
+            a_times_s = MultiplyNTTs(A_hat[i][j], s_hat[j]) #A*s
+            aux = [(aux[t] + a_times_s[t]) % q for t in range(256)] #+t
+        t_hat.append(aux)
+
+    ek = b"".join(ByteEncode_d(t_hat[i], 12) for i in range(k)) + rho
+    dk = b"".join(ByteEncode_d(s_hat[i], 12) for i in range(k))
+    return ek, dk
+
+    # FIPS203 Algorithm 14
+def KPKE_Encrypt(ek: bytes, m: bytes, r: bytes, k: int, eta1: int, eta2: int, du: int, dv: int) -> bytes:
+    # t^
+    t_hat = []
+    for i in range(k):
+        off = 384 * i
+        t_hat.append(ByteDecode_d(ek[off:off + 384], 12))
+
+    rho = ek[384 * k:384 * k + 32]
+
+    # A^ matrix
+    A_hat = [[None for _ in range(k)] for _ in range(k)]
+    for i in range(k):
+        for j in range(k):
+            A_hat[i][j] = SampleNTT(rho + bytes([i]) + bytes([j])) # VERIFICAR
+
+    Nctr = 0
+    y = []
+    for _ in range(k):
+        y.append(SamplePolyCBD_eta(PRF_eta(eta1, r, bytes([Nctr])), eta1))
+        Nctr += 1
+        
+    e1 = []
+    for _ in range(k):
+        e1.append(SamplePolyCBD_eta(PRF_eta(eta2, r, bytes([Nctr])), eta2))
+        Nctr += 1
+
+    e2 = SamplePolyCBD_eta(PRF_eta(eta2, r, bytes([Nctr])), eta2)
+    
+    y_hat = [NTT(poly) for poly in y]
+
+    u = []
+    for i in range(k):
+        aux_hat = [0] * 256
+        for j in range(k):
+            prod = MultiplyNTTs(A_hat[j][i], y_hat[j])  
+            aux_hat = [(aux_hat[t] + prod[t]) % q for t in range(256)]
+        u_ntt_inv = NTT_inv(aux_hat)
+        u.append([(u_ntt_inv[t] + e1[i][t]) % q for t in range(256)]) 
+ 
+    mu = Decompress_d(ByteDecode_d(m,1), 1)
+
+    aux_hat = [0] * 256
+    for j in range(k):
+        prod = MultiplyNTTs(t_hat[j], y_hat[j])
+        aux_hat = [(aux_hat[t] + prod[t]) % q for t in range(256)] 
+    v_ntt_inv = NTT_inv(aux_hat)
+    v = [((v_ntt_inv[t] + e2[t]) % q + mu[t]) % q for t in range(256)] 
+
+    
+    c1 = b"".join(ByteEncode_d(Compress_d(u[i], du), du) for i in range(k))
+    c2 = ByteEncode_d(Compress_d(v, dv), dv)
+
+    c = c1+c2
+    return c
+    
+# FIPS203 Algorithm 15
+def KPKE_Decrypt(dk: bytes, c: bytes, k: int, du: int, dv: int) -> bytes:
+    c1 = c[0:32 * du * k]
+    c2 = c[32 * du * k:32 * du * k + 32 * dv]
+
+    u_ = []
+    for i in range(k):
+        off = i * (32 * du)
+        u_.append(Decompress_d(ByteDecode_d(c1[off:off + 32 * du], du), du))
+
+    v_ = Decompress_d(ByteDecode_d(c2, dv), dv)
+
+    s_hat = []
+    for i in range(k):
+        off = i * 384
+        s_hat.append(ByteDecode_d(dk[off:off + 384], 12))
+
+    aux_hat = [0] * 256
+    for j in range(k):
+        prod = MultiplyNTTs(s_hat[j], NTT(u_[j]))
+        aux_hat = [(aux_hat[t] + prod[t]) % q for t in range(256)]
+    w_temp = NTT_inv(aux_hat)
+    w = [(v_[t] - w_temp[t]) % q for t in range(256)]
+
+    m = ByteEncode_d(Compress_d(w, 1),1)
+
+    return m
---
+++ src/mlkem/internal_mlkem.py
@@ -0,0 +1,33 @@
+from auxiliaries import *
+from internal_kpke import KPKE_KeyGen, KPKE_Encrypt, KPKE_Decrypt
+
+# FIPS203 Algorithm 16
+def INTERNAL_MLKEM_KeyGen(d: bytes, z: bytes, params):
+    ek_pke, dk_pke = KPKE_KeyGen(d, params.k, params.eta1)
+    ek = bytes(ek_pke)
+    dk = dk_pke+ ek+ H(ek)+ z
+    return ek, dk
+    
+# FIPS203 Algorithm 17
+def INTERNAL_MLKEM_Encaps(ek: bytes, m: bytes, params):
+    K, r = G(m + H(ek))
+    c = KPKE_Encrypt(ek, m, r, params.k, params.eta1, params.eta2, params.du, params.dv)
+    return K, c
+    
+# FIPS203 Algorithm 18
+def INTERNAL_MLKEM_Decaps(dk: bytes, c: bytes, params):
+    dk_pke = dk[0:384*params.k]            # PKE decryption key
+    ek_pke = dk[384*params.k:768*params.k+32]     # PKE encryption key
+    h = dk[768*params.k + 32 : 768*params.k + 64] # hash of PKE_ek
+    z = dk[768*params.k + 64 : 768*params.k + 96] # implicit rejection value
+    
+    m_prime = KPKE_Decrypt(dk_pke, c, params.k, params.du, params.dv)
+    K_prime, r_prime = G(m_prime + h)
+    K_ = J(z + c)
+    
+    c_prime = KPKE_Encrypt(ek_pke, m_prime, r_prime, params.k, params.eta1, params.eta2, params.du, params.dv)
+    
+    if c != c_prime: # implicit rejection
+        K_prime = K_
+        
+    return K_prime
